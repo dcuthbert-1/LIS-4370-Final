6fe355cb1916c85fa8b238d29c063764d9a178c9
dcuthbert-1 <54560318+dcuthbert-1@users.noreply.github.com>
2021-04-29 17:32:55 -0400

    Update testScript.R

--- testScript.R
+++ testScript.R
@@ -1,15 +1,20 @@
 library(dBrandsConvert)
+library(dBrandsConvert)
 
 
 
 # No data for testing, must enter it manually. If attempting
-brandSize('woman', 'Fendi', 9)
+brandSize('woman', 'Fendi', 7)
 brandSize('woman', 'givenchy', 7)
-
+brandSize('woman', 'mcq', 7)
+brandSize('woman', 'sw', 7)
+brandSize('woman', 'tom ford', 7)
 
 # This test should produce Error message because there is no Male data yet for this package.
 brandSize('Male', 'givenchy', 7)
 
+# Nike is not a brand that was utilized in this package
+brandSize('woman', 'nike', 7)
 
 
 # No data for testing, must enter it manually. Currently commented out because function is not reading png file properly. But the two most important functions are working.
