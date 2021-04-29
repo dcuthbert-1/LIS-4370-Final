35fdbc1cfca091a0f37f7e8c6aa67c3411750bee
dcuthbert-1 <54560318+dcuthbert-1@users.noreply.github.com>
2021-04-29 17:21:50 -0400

    Add files via upload

---
+++ testScript.R
@@ -0,0 +1,18 @@
+library(dBrandsConvert)
+
+
+
+# No data for testing, must enter it manually. If attempting
+brandSize('woman', 'Fendi', 9)
+brandSize('woman', 'givenchy', 7)
+
+
+# This test should produce Error message because there is no Male data yet for this package.
+brandSize('Male', 'givenchy', 7)
+
+
+
+# No data for testing, must enter it manually. If attempting, should produce png image file
+
+#brandChecker("alexander mcqueen")
+#brandChecker("tom ford")
