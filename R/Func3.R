b10e4c37e920bbb67e58ea0855ef7f392079e210
dcuthbert-1 <54560318+dcuthbert-1@users.noreply.github.com>
2021-04-29 05:57:58 -0400

    Update Func3.R

--- R/Func3.R
+++ R/Func3.R
@@ -9,23 +9,25 @@
 #' @export
 #' @examples
 #' brandChecker('fendi')
+myimages<-list.files("images/", pattern = ".png", full.names = TRUE)
+include_graphics(myimages)
 
 brandChecker<-function(brand ="alexander mcqueen"){
   brand <-trimws(tolower(brand))
   if (brand == "alexander mcqueen"){
-    return(knitr::include_graphics("Checker1.png"))
+    return(knitr::include_graphics("images/Checker1.png"))
   }
   if (brand == "fendi"){
-    return(knitr::include_graphics("Checker2.png"))
+    return(knitr::include_graphics("images/Checker2.png"))
   }
   if (brand == "givenchy"){
-    return(knitr::include_graphics("Checker3.png"))
+    return(knitr::include_graphics("images/Checker3.png"))
   }
   if (brand == "stuart weitzman"){
-    return(knitr::include_graphics("Checker4.png"))
+    return(knitr::include_graphics("images/Checker4.png"))
   }
   if (brand == "tom ford"){
-    return(knitr::include_graphics("Checker5.png"))
+    return(knitr::include_graphics("images/Checker5.png"))
   }
   if (brand %in% c("alexander mcqueen","fendi","givenchy","stuart weitzman", "tom ford") == FALSE){
     return("UPlease check spelling of brand, only accepts full company name, no abbreviations or accronyms")
