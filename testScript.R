4e222d62f8a2a28ba27acfb8cf8c2e56e58e026b
dcuthbert-1 <54560318+dcuthbert-1@users.noreply.github.com>
2021-04-29 17:23:06 -0400

    Update testScript.R

--- testScript.R
+++ testScript.R
@@ -12,7 +12,7 @@ brandSize('Male', 'givenchy', 7)
 
 
 
-# No data for testing, must enter it manually. If attempting, should produce png image file
+# No data for testing, must enter it manually. Currently commented out because function is not reading png file properly. But the two most important functions are working.
 
 #brandChecker("alexander mcqueen")
 #brandChecker("tom ford")
